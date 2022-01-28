b6ba25fcc18a8834e778d5627ccba06c4bd68dca
TuftedDeer <tufteddeer@disroot.org>
2022-01-28 21:11:28 +0100

    basic left right steering

--- Assets/Scripts/Boat.cs
+++ Assets/Scripts/Boat.cs
@@ -1,24 +1,32 @@
-using Unity.VisualScripting;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Boat : MonoBehaviour
 {
+    private Rigidbody rigidbody;
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody>();
     }
     
     void Update()
     {
+        float rowForce = 50.0f;
+        Vector3 torque = Vector3.zero;
+
         if (Input.GetKeyDown(KeyCode.A))
         {
             Debug.Log("left");
+            torque += Vector3.up * rowForce;
         }
 
         if (Input.GetKeyDown(KeyCode.D))
         {
             Debug.Log("right");
+            torque += -Vector3.up * rowForce;
         }
+        
+        rigidbody.AddTorque(torque);
     }
 
 }
