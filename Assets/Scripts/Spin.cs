4c5dd66180e2a440193d29fb7d9cec981193d6a2
Alexander Brandt <45146885+stockhut@users.noreply.github.com>
2022-01-29 16:22:39 +0100

    spinny light

--- Assets/Scripts/Spin.cs
+++ Assets/Scripts/Spin.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
 
 public class Spin : MonoBehaviour
 {
     public float rotationSpeed = 100.0f;
+    public float xAxis;
+    public float yAxis;
+    public float zAxis;
 
     void Update()
     {
-        transform.Rotate(new Vector3(0, rotationSpeed, 0) * Time.deltaTime) ;
+        transform.Rotate(new Vector3(xAxis * rotationSpeed, yAxis * rotationSpeed, zAxis * rotationSpeed) * Time.deltaTime) ;
     }
 }
