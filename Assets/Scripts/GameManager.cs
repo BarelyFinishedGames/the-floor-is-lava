18a9fa8f3e192e5631f0be6b48aa61e8af130f05
Florian Arens <60519307+Flo0807@users.noreply.github.com>
2022-01-29 22:20:41 +0100

    Add preload scene

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,30 +1,37 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.SceneManagement;
 
-public class GameManager
+public class GameManager : MonoBehaviour
 {
-    private static GameManager instance;
-    public static GameManager Instance
-    {
-        get
-        {
-            if (instance is null)
-            {
-                instance = new GameManager();
-            }
+    public static GameManager Instance { get; private set; }
 
-            return instance;
-        }
-    }
+    private static int nextScene = 1;
 
     public readonly UnityEvent OnGameOver = new();
-    private GameManager()
+
+    private void Awake()
     {
-        
+        if (!Instance)
+        {
+            Instance = this;
+
+            DontDestroyOnLoad(gameObject);
+        }
+
+        SceneManager.LoadScene(nextScene);
     }
 
     public void GameOver()
     {
         OnGameOver.Invoke();
+    }
+
+    public static void Load()
+    {
+        nextScene = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(0);
     }
 }
